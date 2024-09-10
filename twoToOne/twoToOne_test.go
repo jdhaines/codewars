e47897920a56ad127de61a83dca03fd3789b838d
Josh Haines <Josh@JoshHaines.com>
2024-09-10 14:57:54 -0400

    feat(benchmarks): add testing benchmarks
    I added benchmarks which you can run with `go test -bench=.` that show
    my solution is almost twice as slow as the smart internet solution.  Sad
    because it's also way more lines than that solution.

--- twoToOne/twoToOne_test.go
+++ twoToOne/twoToOne_test.go
@@ -12,7 +12,7 @@ var test_cases = []struct {
 	{[2]string{"loopingisfunbutdangerous", "lessdangerousthancoding"}, "abcdefghilnoprstu"},
 }
 
-func TestTwoToOne(t *testing.T) {
+func TestTwoToOneJoshSolution(t *testing.T) {
 	for _, tt := range test_cases {
 		result := TwoToOne(tt.in[0], tt.in[1])
 		if tt.out != result {
@@ -20,6 +20,34 @@ func TestTwoToOne(t *testing.T) {
 		} else {
 			t.Logf("expected %v, got %v", tt.out, result)
 			continue
+		}
+	}
+}
+
+func Benchmark_Josh_Solution(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range test_cases {
+			TwoToOne(tt.in[0], tt.in[1])
+		}
+	}
+}
+
+func TestTwoToOneSmartyPantsSolution(t *testing.T) {
+	for _, tt := range test_cases {
+		result := TwoToOne(tt.in[0], tt.in[1])
+		if tt.out != result {
+			t.Errorf("expected %v, got %v", tt.out, result)
+		} else {
+			t.Logf("expected %v, got %v", tt.out, result)
+			continue
+		}
+	}
+}
+
+func Benchmark_Smarty_Pants_Solution(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range test_cases {
+			wishIWasSmart(tt.in[0], tt.in[1])
 		}
 	}
 }
